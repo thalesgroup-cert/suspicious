b6d49385ad6273ae2833460f7c565e250de75fe2
TheoBhang <theo.bhang@thalesgroup.com>
2026-04-05 15:07:58 +0200

    fixed s3 error

--- email-feeder/main.py
+++ email-feeder/main.py
@@ -142,7 +142,7 @@ def main() -> int:
         )
     )
 
-    minio_service = classes.services.minio_service.MinioService(config.s3)
+    minio_service = classes.services.minio_service.MinioService(config.minio)
     try:
         minio_service.connect()
         logger.info("MinIO client initialized from config.")
